60554303e1a9e06397d4214cd968549ce37cd0cb
w.hunt <wfh2d88@gmail.com>
2021-02-22 13:57:29 -0600

    added some solid pseudocode as well as prompt and generate framework

--- Index.js
+++ Index.js
@@ -1,5 +1,28 @@
+const inquirer = require("inquirer")
+
+
+
 // prompts for managers 'Name', 'ID', 'Email', 'Office Number'
+const managerPrompt = () => {
+    return inquirer.prompt()
 
-// menu with option to 'add engineer or intern' or 'finish building team'
+    // menu with option to 'add engineer or intern' or 'finish building team'
+};
 
-// add enginer? Y/N. If yes 'Name', 'ID', 'Email', 'Github'
+const secondaryPrompt = (promptInfo) => {
+    // add enginer? Y/N. If yes 'Name', 'ID', 'Email', 'Github'
+    // type: confirm
+    // type: input
+
+    // add intern? Y/N. If yes 'Name', 'ID', 'Email', 'Github'
+    // type: confirm
+    // type: input
+
+}
+
+
+managerPrompt()
+    .then(secondaryPrompt)
+    .then(promptInfo => {
+        return generatePage(promptInfo);
+    })
---
+++ src/page-template.js
@@ -0,0 +1,8 @@
+const generatePage = require("./src/page-template");
+
+// 2 other functions for creating template literals for Manager info and Secondary employee info
+
+
+module.exports = (templateData) => {
+    // template literals referencing 'templateData' and other functions
+}
