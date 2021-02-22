d5a59287acf73c657d3d6538a15e237c260323ae
w.hunt <wfh2d88@gmail.com>
2021-02-22 09:53:53 -0600

    initial HTML template

---
+++ Index.js
@@ -0,0 +1,5 @@
+// prompts for managers 'Name', 'ID', 'Email', 'Office Number'
+
+// menu with option to 'add engineer or intern' or 'finish building team'
+
+// add enginer? Y/N. If yes 'Name', 'ID', 'Email', 'Github'
