bccc9b4a8a895436bee0af6ef08f56d792f476dd
w.hunt <wfh2d88@gmail.com>
2021-02-26 12:52:51 -0600

    class constructors utilized and HTML generating

--- Index.js
+++ Index.js
@@ -1,68 +1,155 @@
-const inquirer = require("inquirer")
-const generatePage = require("./src/page-template");
-
-
+const inquirer = require("inquirer");
+const employees = [];
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const fs = require('fs')
+const createHtml = require('./src/page-template.js')
+const path = require('path')
 
 // prompts for managers 'Name', 'ID', 'Email', 'Office Number'
-const managerPrompt = () => {
-    return inquirer.prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is the managers name?'
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is the managers ID #?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is the managers email address?'
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'What is the managers office number?'
-    },
-     // menu with option to 'add engineer or intern' or 'finish building team'
-     {
-         type: 'checkbox',
-         name: 'finishOr',
-         choices: [
-             'add engineer or intern?',
-             'finish building team'
-         ]
-
-     }
-
-
-
-
-
-
-
-])
-};
-
-const secondaryPrompt = (promptInfo) => {
-    // add enginer? Y/N. If yes 'Name', 'ID', 'Email', 'Github'
-    // type: confirm
-    // type: input
-
-    // add intern? Y/N. If yes 'Name', 'ID', 'Email', 'Github'
-    // type: confirm
-    // type: input
-
+function managerPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the managers name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the managers ID #?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the managers email address?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the managers office number?",
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      employees.push(manager);
+      secondaryPrompt();
+    });
 }
 
+function secondaryPrompt() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "whatNext",
+      message: "What would you like to do next?",
+      choices: ["add engineer", "add intern", "finish building team"],
+    })
+    .then((answer) => {
+      switch (answer.whatNext) {
+        case "add engineer":
+          engineerPrompt();
+          break;
+        case "add intern":
+          internPrompt();
+          break;
+        default:
+          buildTeam();
+      }
+    });
+}
 
-managerPrompt()
-    .then(promptInfo => {
-        console.log(promptInfo)
-        return generatePage(promptInfo);
-    })
-    .then(pageHTML => {
-        console.log(pageHTML)
-    })
+function engineerPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the engineers name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the engineers ID #?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the engineers email address?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the engineers github username?",
+      },
+    ])
+    .then((answers) => {
+      const engineer = new Engineer(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.github
+      );
+      employees.push(engineer);
+      secondaryPrompt();
+    });
+}
+
+const internPrompt = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the interns name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the interns ID #?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the interns email address?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What is the interns alma mater?",
+      },
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.school
+      );
+      employees.push(intern);
+      secondaryPrompt();
+    });
+};
+
+
+function buildTeam () {
+    fs.writeFileSync("./dist/team.html", createHtml(employees))
+    copyFile()
+};
+
+function copyFile () {
+  fs.copyFileSync("./src/style.css", "./dist/style.css")
+};
+
+
+
+
+managerPrompt();
+
--- src/page-template.js
+++ src/page-template.js
@@ -1,51 +1,95 @@
-const generateManager = (name, id, email, office) => {
-    console.log()
-    return `
-    <div class="card justify-center info-card">
-    <div class="card-body">
-      <h4 class="card-title">${name}</h4>
-      <h5 class="card-subtitle"> Manager </h5>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">${id}</li>
-        <li class="list-group-item">${office}</li>
-        <li class="list-group-item">${email}</li>
-      </ul>
-    </div>
-  </div
-    `
+const Manager = require('../lib/Manager')
+var html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Team-Info Gen</title>
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <header><h1>My Team</h1></header>
+    <main class="d-flex justify-content-center">
+      <div class="row wrap">`
+
+function createHtml (arr) {
+  for(var i = 0; i < arr.length; i++){
+    var role = arr[i].getRole()
+    switch(role) {
+      case "Manager":
+        managerCard(arr[i])
+        break;
+      case 'Intern':
+      internCard(arr[i])
+        break;
+      default:
+        engineerCard(arr[i])
+    }
+  }
+  return html +=       `</div>
+                    </main>
+                  </body>
+                </html>`
+};
+
+function managerCard (manager) {
+
+  html += `        <div class="card justify-center info-card">
+  <div class="card-body">
+    <h4 class="card-title">${manager.name}</h4>
+    <h5 class="card-subtitle">Manager</h5>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">${manager.id}</li>
+      <li class="list-group-item">${manager.officeNumber}</li>
+      <li class="list-group-item">${manager.email}</li>
+    </ul>
+  </div>
+</div>`
+};
+
+function engineerCard (engineer) {
+
+  html += `        <div class="card justify-center info-card">
+  <div class="card-body">
+    <h4 class="card-title">${engineer.name}</h4>
+    <h5 class="card-subtitle">Engineer</h5>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">${engineer.id}</li>
+      <li class="list-group-item">${engineer.officeNumber}</li>
+      <li class="list-group-item">${engineer.email}</li>
+    </ul>
+  </div>
+</div>`
+};
+
+function internCard (intern) {
+
+  html += `        <div class="card justify-center info-card">
+  <div class="card-body">
+    <h4 class="card-title">${intern.name}</h4>
+    <h5 class="card-subtitle">Intern</h5>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">${intern.id}</li>
+      <li class="list-group-item">${intern.school}</li>
+      <li class="list-group-item">${intern.email}</li>
+    </ul>
+  </div>
+</div>`
 }
 
 
 
-// 2 other functions for creating template literals for Manager info and Secondary employee info
+// function to take in array of employees/ all employees. each employee it runs getRole(). if manager, call generate manager, if engineer etc.
+
+// 3 functions for creating template literals for Manager info and Secondary employee info
 
 
-module.exports = (templateData) => {
-    // template literals referencing 'templateData' and other functions
-    console.log(templateData)
-    const {name, id, email, office} = templateData
-    return`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Team-Info Gen</title>
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossorigin="anonymous"
-        />
-        <link rel="stylesheet" href="style.css" />
-      </head>
-      <body>
-        <header><h1>My Team</h1></header>
-        <main class="d-flex justify-content-center">
-          <div class="col-2 wrap">
-            ${generateManager(name, id, email, office )}
-          </div>
-        </main>
-      </body>
-    </html>`
-}
+
+module.exports = createHtml;
